8f0b0ecba93cf1bd07a810ebbced3987a3960e02
SHUBHAM-DEVGEEK <shubhambhardwaj8812@gmail.com>
2026-04-01 16:58:41 +0530

    tiling problrm

---
+++ Tiling.java
@@ -0,0 +1,15 @@
+public class Tiling {
+    public static int tilling(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        int fnm1= tilling(n-1);
+        int fnm2=tilling(n-2);
+        int total = fnm1 +fnm2;
+        return total;
+    }
+    public static void main(String[] args) {
+        System.out.println(tilling(4));
+    }
+    
+}
